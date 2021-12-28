fc4861ba8437a63a78c5f98216581280857b9193
Jenna <ruaaaa@ajou.ac.kr>
2021-12-28 15:56:51 +0900

    Initial settings

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Markdown example post",
+        "excerpt":"⭐️Just for the example of the markdown⭐️ Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered...","categories": ["etc"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/etc/markdown-example/",
+        "teaser": null
+      },{
+        "title": "샘플",
+        "excerpt":"카테고리 반영 잘 되는지 샘플 포스팅..!  ","categories": ["Git"],
+        "tags": [],
+        "url": "/git/sample/",
+        "teaser": null
+      },{
+        "title": "리눅스 커널 - 시스템 구성정보 확인하기",
+        "excerpt":"    공부한거 정리할겸 블로그 공부도 할겸 포스팅   내용은 책 리눅스 커널 이야기를 보고 정리   밍은 사진도 넣고싶은데,,, 넘 어려워요 어캐 넣는지 몰겠어서 차후 수정 ㅎ       시스템 구성 정보 확인하기  1. 커널 정보 확인하기      버전확인  uname 명령어 사용  uname -a  커널 버전 정보 확인 가능!       ","categories": ["Linux"],
+        "tags": [],
+        "url": "/linux/Linux-Kernel1/",
+        "teaser": null
+      }]
